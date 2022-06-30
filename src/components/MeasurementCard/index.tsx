c4e7e57f6edbfca8b862693864ee81c61f3f669a
Arthur Moreira de Deus <arthurdedeus@gmail.com>
2022-06-29 22:49:11 -0300

    feat: Add graph to measurements card with mock data

--- src/components/MeasurementCard/index.tsx
+++ src/components/MeasurementCard/index.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React from 'react';
+import { Dimensions } from 'react-native';
+import { LineChart } from 'react-native-chart-kit';
+
 import {
   Container,
   Title,
@@ -9,7 +12,7 @@ import {
   ContentWrapper,
   GraphWrapper,
   Graph,
-} from "./styles";
+} from './styles';
 
 export interface MeasurementCardProps {
   type: string;
@@ -26,6 +29,31 @@ interface Props {
 }
 
 export function MeasurementCard({ data, onPress }: Props) {
+  const width = 185;
+  const height = 100;
+  const data2 = {
+    labels: [],
+    datasets: [
+      {
+        data: [50, 20, 2, 86, 71, 100],
+        color: (opacity = 1) => `rgba(18, 164, 84, ${opacity})`,
+      },
+    ],
+  };
+  const chartConfig = {
+    backgroundColor: '#fff',
+    backgroundGradientFrom: '#fff',
+    backgroundGradientTo: '#fff',
+    decimalPlaces: 1,
+    color: (opacity = 1) => `rgba(18, 164, 84, ${opacity})`,
+    style: {
+      borderRadius: 0,
+    },
+  };
+  const graphStyle = {
+    marginVertical: 0,
+    ...chartConfig.style,
+  };
   return (
     <Container onPress={onPress}>
       <ContentWrapper>
@@ -39,9 +67,19 @@ export function MeasurementCard({ data, onPress }: Props) {
           </Timestamp>
         </Footer>
       </ContentWrapper>
-      <GraphWrapper>
-        <Graph source={require("../../assets/images/graph-small.png")} />
-      </GraphWrapper>
+      {/* <GraphWrapper> */}
+        <LineChart
+          data={data2}
+          width={width}
+          height={height}
+          chartConfig={chartConfig}
+          style={graphStyle}
+          // withHorizontalLabels={false}
+          withInnerLines={false}
+          withOuterLines={false}
+        />
+        {/* <Graph source={require("../../assets/images/graph-small.png")} /> */}
+      {/* </GraphWrapper> */}
     </Container>
   );
 }
